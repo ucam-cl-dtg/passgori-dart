337c5feea6ff9f5036a0d75b092af3d71a4edf7d
Daniel Thomas <drt24@cam.ac.uk>
2012-12-03 11:16:49 +0000

    Refactor NigoriConstants to be static
    Signed-off-by: Daniel Thomas <drt24@cam.ac.uk>

--- bin/PassgoriExtension.dart
+++ bin/PassgoriExtension.dart
@@ -1,25 +1,39 @@
 
 import 'dart:html';
+import 'dart:scalarlist';
+import 'dart:utf';
 import 'package:js/js.dart' as js;
 
 /**
  * Refer to the Constants section of the RFC for the meaning of these constants
  **/
 class NigoriConstants {
-  final int Nsalt = 1000;
-  final int Nuser = 1001;
-  final int Nenc = 1002;
-  final int Nmac = 1003;
-  final int Niv = 1004;
-  final int Nmaster = 1005;
-  final int Bsuser = 16;
-  final int Bkdsa = 16;
-  final int Bkenc = 16;
-  final int Bkmac = 16;
-  final int Bkiv = 16;
-  final int Bkmaster = 16;
-}
+  static final int Nsalt = 1000;
+  static final int Nuser = 1001;
+  static final int Nenc = 1002;
+  static final int Nmac = 1003;
+  static final int Niv = 1004;
+  static final int Nmaster = 1005;
+  static final int Bsuser = 16;
+  static final int Bkdsa = 16;
+  static final int Bkenc = 16;
+  static final int Bkmac = 16;
+  static final int Bkiv = 16;
+  static final int Bkmaster = 16;
+  static final Usersalt = new Uint8List(9);
 
+  static void init() {
+  Usersalt[0] = 117;
+  Usersalt[1] = 115;
+  Usersalt[2] = 101;
+  Usersalt[3] = 114;
+  Usersalt[4] = 32;
+  Usersalt[5] = 115;
+  Usersalt[6] = 97;
+  Usersalt[7] = 108;
+  Usersalt[8] = 116;
+  }
+}
 
 void main() {
   js.scoped(() {
@@ -34,6 +48,6 @@ void main() {
         js.context.alert('ciphertext: $ciphertext plaintext: $plaintext');
         });
       });
-    
+    NigoriConstants.init();
   });
 }
