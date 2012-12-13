7f5297613eb68ff78bcc9f9826f984f7a921d3f4
Daniel Thomas <drt24@cam.ac.uk>
2012-12-13 14:50:43 +0000

    Add testing on passgori
    Signed-off-by: Daniel Thomas <drt24@cam.ac.uk>

---
+++ test/passgori_test.dart
@@ -0,0 +1,11 @@
+import 'package:unittest/unittest.dart';
+import 'package:unittest/html_config.dart';
+import 'package:PassgoriExtension/keys.dart';
+import 'package:nigori/nigori.dart';
+
+void main() {
+  NigoriConstants.init();
+  useHtmlConfiguration();
+  test('constants salt not null', () => expect(NigoriConstants.Usersalt,isNotNull));
+  test('keyderivation', () => new NigoriKeys.derive("test","test","test"));
+}
